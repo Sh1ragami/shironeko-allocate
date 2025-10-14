96921c08277ede5fe93c0c3875276861b387b62e
白神晴陽 <aoshi09275398@icloud.com>
2025-10-14 12:29:04 +0900

    add: ルーティング機能の土台を作成

---
+++ frontend/src/pages/login.ts
@@ -0,0 +1,6 @@
+export function renderLogin(container: HTMLElement): void
+{
+  container.innerHTML = `
+    <h1>ここにログイン画面を書く</h1>
+  `
+}
---
+++ frontend/src/pages/top.ts
@@ -0,0 +1,6 @@
+export function renderTop(container: HTMLElement): void
+{
+  container.innerHTML= `
+    <h1>ここにトップページの画面を書く</h1>
+  `
+}
